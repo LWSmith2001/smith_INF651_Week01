f3241bc85be7a8eadff3791070293c885478ab89
LWSmith2001 <LWSmith2001@gmail.com>
2022-09-14 08:28:11 -0500

    Added a forgotten line

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let myTotal = myNumber * myOtherNumber;
 
 let myName = "";
 let myCombo = "" + myNumber + myOtherNumber;
+let myOtherCombo = myNumber + myOtherNumber;
 let myRemainder = myNumber % 5;
 let i = 2;
 let int = 4;
