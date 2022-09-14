c92acb58fccdf1df5d2c59c5daddf14b380aaab2
LWSmith2001 <LWSmith2001@gmail.com>
2022-09-14 08:18:33 -0500

    adding main.js for Assignment 1.

---
+++ main.js
@@ -0,0 +1,9 @@
+let myNumber = 5, myOtherNumber = 5;
+let myTotal = myNumber * myOtherNumber;
+
+let myName = "";
+let myCombo = "" + myNumber + myOtherNumber;
+let myRemainder = myNumber % 5;
+let i = 2;
+let int = 4;
+let myBoolean = false;
